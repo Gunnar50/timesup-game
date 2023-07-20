ac38bafeeffedea74e741e68f65826edde79f048
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-07-20 16:50:16 +0100

    add urls

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.WaitingRoomView.as_view()),
+    path("room", views.RoomView.as_view()),
+    path("create-room", views.CreateRoomView.as_view()),
+    path("get-room", views.GetRoom.as_view()),
+    path("join-room", views.JoinRoom.as_view()),
+    path("user-in-room", views.UserInRoom.as_view()),
+    path("exit-room", views.ExitRoom.as_view()),
+    path("update-room", views.UpdateRoom.as_view()),
 ]
