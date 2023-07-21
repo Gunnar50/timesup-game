deccca4bd1cb84cb66bcaf56d8ef63eff9bce814
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-07-21 08:14:26 +0100

    fixed join room

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics, status
-from .serializers import RoomSerializer, CreateRoomSerializer, UpdateRoomSerializer, UserSerializer
+from .serializers import RoomSerializer, CreateRoomSerializer, UpdateRoomSerializer, UserSerializer, CreateUserSerializer
 from .models import Room, User
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -13,7 +13,8 @@ class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     # serialize the class
     serializer_class = RoomSerializer
-    
+
+
 class GetRoom(APIView):
     serializer_class = RoomSerializer
     lookup_url_kwargs = "code" # code matches the code variable in Room class models.py
@@ -22,57 +23,76 @@ class GetRoom(APIView):
         # use requests.GET -> for GET requests
         code = request.GET.get(self.lookup_url_kwargs)
         if code != None:
-            query_room = Room.objects.filter(code=code)
-            if query_room.exists():
-                room = query_room[0]
-                data = RoomSerializer(room).data
-                data["is_host"] = self.request.session.session_key == room.host
-                return Response(data, status=status.HTTP_200_OK)
-            return Response({"Room Not Found": "Invalid Room Code."}, status=status.HTTP_404_NOT_FOUND)
+            session_id = self.request.session.session_key
+            user_query = User.objects.filter(session_id=session_id)
+            if user_query.exists():
+                user = user_query[0]
+                query_room = Room.objects.filter(code=code)
+                if query_room.exists():
+                    room = query_room[0]
+                    if user.room == room:
+                        data = RoomSerializer(room).data
+                        data["is_host"] = user.is_host
+                        return Response(data, status=status.HTTP_200_OK)
+                    return Response({"Incorrect Room": "User Must Leave The Room First"}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({"Room Not Found": "Invalid Room Code"}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"User Not Found": "User must joing a room first"}, status=status.HTTP_404_NOT_FOUND)
         return Response({"Bad Request": "Code parameter not found in request."}, status=status.HTTP_400_BAD_REQUEST)
 
 class JoinRoom(APIView):
-    lookup_url_kwargs = "code"
-    
     def post(self, request, format=None):
         # if current user does not have an active session with the server
-        if not self.request.session.exists(self.request.session.session_key):
+        session_id = self.request.session.session_key
+        print(1, session_id)
+        if self.request.session.exists(session_id):
+            if User.objects.filter(session_id=session_id).exists():
+                return Response({"Not Allowed": "User can only be in on room at one time."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        else:
             # create a session
             self.request.session.create()
+            session_id = self.request.session.session_key
+        print(2, session_id)
             
         # request.data -> use data for post requests.
-        code = request.data.get(self.lookup_url_kwargs)
-        if code:
-            query_room_result = Room.objects.filter(code=code)
-            if query_room_result.exists():
-                room = query_room_result[0]
-                
-                # this new item room code is to check later if the user reconnect to the app then the user
-                # do not need to type the room code again
-                self.request.session["room_code"] = code
-                
-                return Response({"message": "Room Joined!"}, status=status.HTTP_200_OK)
-            
+        username = request.data.get("username")
+        if User.objects.filter(username=username).exists():
+            return Response({"Not Allowed": "Username Must Be Unique."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        code = request.data.get("code")
+        if not code:
+            return Response({"Bad Request": "Code parameter not found in request."}, status=status.HTTP_400_BAD_REQUEST)
+
+        query_room = Room.objects.filter(code=code)
+        if not query_room.exists():
             return Response({"Room Not Found": "Invalid Room Code."}, status=status.HTTP_404_NOT_FOUND)
+        
+        # this new item room code is to check later if the user reconnect to the app then the user
+        # do not need to type the room code again
+        self.request.session["room_code"] = code
+        
+        room = query_room[0]
+        User.objects.create(username=username, session_id=session_id, room=room, is_host=False)
+        
+        return Response({"message": "Room Joined!"}, status=status.HTTP_200_OK)
+        
+       
             
-        return Response({"Bad Request": "Code parameter not found in request."}, status=status.HTTP_400_BAD_REQUEST)
-
-
-
 class CreateRoomView(APIView):
-    serializer_class = CreateRoomSerializer
+    room_serializer_class = CreateRoomSerializer
+    user_serializer_class = CreateUserSerializer
     
     def post(self, request, format=None):
         # if current user does not have an active session with the server
         if not self.request.session.exists(self.request.session.session_key):
             # create a session
             self.request.session.create()
-            
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
+        
+        room_serializer = self.room_serializer_class(data=request.data)
+        user_serializer = self.user_serializer_class(data=request.data)
+        if room_serializer.is_valid() and user_serializer.is_valid():
             # create a room
-            username = serializer.data.get("username")
-            words_per_user = serializer.data.get("words_per_user")
+            username = user_serializer.data.get("username")
+            words_per_user = room_serializer.data.get("words_per_user")
             host_id = self.request.session.session_key
             queryset = User.objects.filter(session_id=host_id, is_host=True)
             
@@ -104,12 +124,12 @@ class CreateRoomView(APIView):
                 
                 return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
                 
-        return Response(RoomSerializer(room).data, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"Bad Request": "Room Not Found"}, status=status.HTTP_400_BAD_REQUEST)
     
-class RoomUsersView(APIView):
+class GetUsersInRoom(APIView):
     def get(self, request, format=None):
-        room_code = request.GET.get("room_code")
-        room = Room.objects.filter(code=room_code)
+        code = request.GET.get("code")
+        room = Room.objects.filter(code=code)
         if room.exists():
             users = User.objects.filter(room=room[0])
             return Response(UserSerializer(users, many=True).data, status=status.HTTP_200_OK)
@@ -133,12 +153,15 @@ class ExitRoom(APIView):
     def post(self, request, format=None):
         if "room_code" in self.request.session:
             self.request.session.pop("room_code")
-            host_id = self.request.session.session_key
-            query_room_results = Room.objects.filter(host=host_id)
-            if query_room_results.exists():
-                room = query_room_results[0]
-                room.delete()
-                
+            session_id = self.request.session.session_key
+            query_user = User.objects.filter(session_id=session_id)
+            if query_user.exists():
+                user = query_user[0]
+                if user.is_host:
+                    room = query_user[0].room
+                    room.delete()
+                user.delete()
+       
         return Response({"Message": "Success"}, status=status.HTTP_200_OK)
     
     
