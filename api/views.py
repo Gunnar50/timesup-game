2d16244eab9abec7a486219ebd1d5cb476412120
Gunnar <gustavo.passarella@hotmail.com>
2023-07-23 10:07:32 +0100

    small changes

--- api/views.py
+++ api/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from django.http import JsonResponse
 from django.middleware.csrf import get_token
 
+
 def csrf(request):
     return JsonResponse({'csrfToken': get_token(request)})
 
--- frontend/urls.py
+++ frontend/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('csrf/', views.csrf, name='csrf'),
     path('room/<int>:id/', views.index),
     path('join-room/', views.index),
     path('create-room/', views.index),
