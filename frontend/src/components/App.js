47573b43ae53c0c1b143d3b181107d348b40c871
Gunnar <gustavo.passarella@hotmail.com>
2023-07-23 10:53:54 +0100

    changed the routes for home instead of app

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,21 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import {render} from "react-dom";
-import Room from "./Room";
-import CreateRoom from "./CreateRoom";
-import JoinRoom from "./JoinRoom";
 import Home from "./Home";
 
 export default function App(){
     return (
-        <Router>
-            <Routes>
-                <Route exact path='/' element={<Home/>} />
-                <Route path='/create-room' element={<CreateRoom/>} />
-                <Route path='/join-room' element={<JoinRoom/>} />
-            </Routes>      
-         
-        </Router>
+        <Home/>
     );
 }
 
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {render} from "react-dom";
+import Room from "./Room";
+import CreateRoom from "./CreateRoom";
+import JoinRoom from "./JoinRoom";
 
 function Home() {
   return (
-    <div>
-      <h1>Home!</h1>
-    </div>
+    <Router>
+        <Routes>
+            <Route exact path='/' element={<Home/>} />
+            <Route path='/create-room' element={<CreateRoom/>} />
+            <Route path='/join-room' element={<JoinRoom/>} />
+            <Route path='/room/:code' element={<Room/>} />
+        </Routes>      
+         
+    </Router>
   )
 }
 
