be51081058fbcea9a80ec5f86343c4ad178b1390
Gunnar <gustavo.passarella@hotmail.com>
2023-07-23 14:16:31 +0100

    add getdetails and get users

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 function Room() {
-  return (
-    <div>
-      <h1>Room: Code</h1>
-    </div>
-  )
+	const code = useParams()["code"];
+	const [details, setDetails] = useState(null);
+	const [users, setUsers] = useState([]);
+
+	const getRoomDetails = () => {
+		fetch(`/api/get-room?code=${code}`)
+		.then(res => res.json())
+		.then(data => setDetails(data));
+	};
+
+	const getUsers = () => {
+		fetch(`/api/get-users?code=${code}`)
+		.then(res => res.json())
+		.then(data => setUsers(data));
+	};
+
+	useEffect(() => (
+		getRoomDetails(),
+		getUsers()
+	), []);
+
+	return (
+		<div>
+			<h2>Room Code: {code}</h2>
+
+
+			<h4>Users in the room:</h4>
+			<ul>
+				{users.map((user) => (
+					<li>{user.username}</li>
+				))}
+			</ul>
+
+		</div>
+	)
 }
 
 export default Room
