a9b910f26f7aafbb1ab3cec2e050c23f0cb3587e
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-07-24 09:02:17 +0100

    updated details and added exit button

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -1,14 +1,22 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 
-function Room() {
-	const code = useParams()["code"];
+function Room(props) {
+	const intervalRef = useRef();
+	const navigate = useNavigate();
+	const {code} = useParams();
 	const [details, setDetails] = useState(null);
 	const [users, setUsers] = useState([]);
 
 	const getRoomDetails = () => {
 		fetch(`/api/get-room?code=${code}`)
-		.then(res => res.json())
+		.then((res) => {
+			if(!res.ok) {
+				props.leaveRoomCallBack();
+				navigate("/join-room");
+			} else return res.json();
+		})
 		.then(data => setDetails(data));
 	};
 
@@ -18,20 +26,29 @@ function Room() {
 		.then(data => setUsers(data));
 	};
 
-	useEffect(() => (
-		getRoomDetails(),
-		getUsers()
-	), []);
+	useEffect(() => getRoomDetails(), []);
+
+	useEffect(() => {
+        intervalRef.current = setInterval(getUsers, 1000);
+        return () => {
+            clearInterval(intervalRef.current);
+        }
+    }, []);
+
+
+	if(!details) return null;
 
 	return (
 		<div>
 			<h2>Room Code: {code}</h2>
+			{details.is_host && <button>Start</button>}
 
+			<p>When the game starts you have to write {details.words_per_user} words.</p>
 
 			<h4>Users in the room:</h4>
 			<ul>
-				{users.map((user) => (
-					<li>{user.username}</li>
+				{users.map((user, index) => (
+					<li key={index}>{user.username}</li>
 				))}
 			</ul>
 
