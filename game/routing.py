cd81f9ea074e3c6c0257f954be052eddebf327b7
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-07-26 18:30:59 +0100

    tesing routing

---
+++ game/routing.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .consumers import GameConsumer  # The consumers module will be defined later
+
+ws_urlpatterns = [
+    # path(r'ws/game/(?P<room_code>\w+)/$', GameConsumer.as_asgi()),  # 'ws/game/ROOM_CODE/'
+    path('ws/game/', GameConsumer.as_asgi()),  # 'ws/game/ROOM_CODE/'
+]
+
+
