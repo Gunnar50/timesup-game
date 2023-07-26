4895d3b5bee1ec02212c30a0c5c72000aa4c5b98
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-07-26 10:38:42 +0100

    updated routing

--- times_up/routing.py
+++ times_up/routing.py
@@ -1,7 +1,7 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import re_path
 
-from your_game_app import consumers  # The consumers module will be defined later
+from game import consumers  # The consumers module will be defined later
 
 websocket_urlpatterns = [
     re_path(r'ws/game/(?P<room_code>\w+)/$', consumers.GameConsumer.as_asgi()),  # 'ws/game/ROOM_CODE/'
