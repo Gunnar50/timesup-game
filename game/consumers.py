a5ada5c879b1937ed404aa2ae5eee9911d453863
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-07-29 08:32:50 +0100

    websocket

--- game/consumers.py
+++ game/consumers.py
@@ -1,30 +1,54 @@
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 import json
-from time import sleep
+from asyncio import sleep
 import random
 
-class GameConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
-        
-        for i in range(1000):
-            self.send(json.dumps({"value": random.randint(-20, 20)}))
-            sleep(1)
-    
-    
+class GameConsumer(AsyncJsonWebsocketConsumer):
     # async def connect(self):
-    #     self.room_code = self.scope['url_route']['kwargs']['room_code']
-    #     await self.channel_layer.group_add(
-    #         self.room_code,
-    #         self.channel_name
-    #     )
     #     await self.accept()
+        
+    #     for _ in range(1000):
+    #         await self.send(json.dumps({"message": random.randint(-20, 20)}))
+    #         await sleep(1)
+    
+    
+    async def connect(self):
+        self.room_code = self.scope['url_route']['kwargs']['room_code']
+        await self.channel_layer.group_add(
+            self.room_code,
+            self.channel_name
+        )
+        await self.accept()
 
-    # async def disconnect(self, close_code):
-    #     await self.channel_layer.group_discard(
-    #         self.room_code,
-    #         self.channel_name
-    #     )
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_code,
+            self.channel_name
+        )
+        
+    async def receive_json(self, content):
+        command = content.get("command", None)
+        
+        if command == "joined":
+            await self.channel_layer.group_send(
+                self.room_code,
+                {
+                    "type": "websocket_joined",
+                    "user": content.get("user", None),
+                    "command":command
+                }
+            )
+            
+        if command == "message":
+            # Send message to room group
+            await self.channel_layer.group_send(
+                self.room_code,
+                {
+                    'type': 'game_message',
+                    'message': content.get("message", None),
+                    "command":command
+                }
+            )
 
     # async def receive(self, text_data):
     #     text_data_json = json.loads(text_data)
@@ -39,10 +63,16 @@ class GameConsumer(WebsocketConsumer):
     #         }
     #     )
 
-    # async def game_message(self, event):
-    #     message = event['message']
+    async def game_message(self, event):
+        message = event['message']
 
-    #     # Send message to WebSocket
-    #     await self.send(text_data=json.dumps({
-    #         'message': message
-    #     }))
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
+        
+    async def websocket_joined(self, event):
+        await self.send_json(({
+             'command':event["command"],
+             'user':event["user"]
+        }))
