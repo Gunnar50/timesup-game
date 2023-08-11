8c0a7e7911559edf79bfdebc6684d46611a439f6
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-08-11 19:54:23 +0100

    small update

--- frontend/src/components/GetCSRF.js
+++ frontend/src/components/GetCSRF.js
@@ -2,6 +2,6 @@ const getCsrfToken = () => {
     return fetch('/api/csrf')
         .then(response => response.json())
         .then(data => data.csrfToken);
-};
+}; 
 
 export default getCsrfToken;
